9e7fdae03e4b651be4a291a3935063003cbd9b83
Recogerous <2737936634@qq.com>
2025-01-02 10:24:14 +0800

    Site updated: 2025-01-02 10:24:13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735777525177";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735783836810";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
